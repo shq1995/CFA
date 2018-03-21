e536a0e99a8493ff957e0662e4e2af738262f150
shq1995 <30364504+shq1995@users.noreply.github.com>
2018-03-21 14:43:49 +0800

    Spring Boot测试类

---
+++ cfa/src/main/java/com/shq/cfa/contoller/TestContoller.java
@@ -0,0 +1,24 @@
+package com.shq.cfa.contoller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shuihuaqi
+ * @create 2018-03-21 14:26
+ * @desc Spring Boot 测试
+ **/
+@RestController
+@EnableAutoConfiguration
+public class TestContoller {
+  @RequestMapping("/hello")
+  public String hello(){
+    return "hello";
+  }
+  public static void main(String[] args){
+    //运行
+    SpringApplication.run(TestContoller.class,args);
+  }
+}
